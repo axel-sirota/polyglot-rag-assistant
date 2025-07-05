e344f64a0e191a27e58b79853cb65c878ccf362a
Axel Sirota <axel.sirota@gmail.com>
2025-07-04 22:18:48 -0300

    All deployment fixes without .terraform files
    - Make certificates optional for demo
    - Add API key variables
    - Remove duplicate CloudFront
    - Use terraform.auto.tfvars
    - Switch to Vercel for UI
    - Fix dependencies
    - Add deployment scripts
    - Remove .terraform from git

--- terraform/modules/s3_static_site/main.tf
+++ terraform/modules/s3_static_site/main.tf
@@ -1,5 +1,11 @@
 resource "aws_s3_bucket" "site" {
   bucket = "${var.project_name}-${var.environment}-ui"
+  
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-ui"
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "site" {
@@ -28,89 +34,5 @@ resource "aws_s3_bucket_website_configuration" "site" {
   
   error_document {
     key = "error.html"
-  }
-}
-
-data "aws_iam_policy_document" "site" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.site.arn}/*"]
-    
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-    
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceArn"
-      values   = [aws_cloudfront_distribution.site.arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "site" {
-  bucket = aws_s3_bucket.site.id
-  policy = data.aws_iam_policy_document.site.json
-  
-  depends_on = [aws_cloudfront_distribution.site]
-}
-
-resource "aws_cloudfront_origin_access_control" "site" {
-  name                              = "${var.project_name}-${var.environment}-oac"
-  description                       = "OAC for ${var.project_name} ${var.environment}"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-resource "aws_cloudfront_distribution" "site" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "${var.project_name} ${var.environment} UI"
-  default_root_object = "index.html"
-  aliases             = var.domain_name != "" ? [var.domain_name] : []
-  price_class         = "PriceClass_100"
-  
-  origin {
-    domain_name              = aws_s3_bucket.site.bucket_regional_domain_name
-    origin_id                = "S3-${aws_s3_bucket.site.id}"
-    origin_access_control_id = aws_cloudfront_origin_access_control.site.id
-  }
-  
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.site.id}"
-    
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-    
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-  
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-  
-  viewer_certificate {
-    cloudfront_default_certificate = var.domain_name == "" ? true : false
-    acm_certificate_arn            = var.domain_name != "" ? var.certificate_arn : null
-    ssl_support_method             = var.domain_name != "" ? "sni-only" : null
-  }
-  
-  tags = {
-    Name        = "${var.project_name}-${var.environment}-cloudfront"
-    Environment = var.environment
-    Project     = var.project_name
   }
 }
